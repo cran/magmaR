f7b16f1cf5000f226729935b87e9d6fcb0668ad6
Daniel Bunis <daniel.bunis@ucsf.edu>
2021-10-02 12:40:13 +0000

    version 1.0.2

--- inst/doc/Upload.R
+++ inst/doc/Upload.R
@@ -14,9 +14,92 @@ vcr::vcr_configure(
 insert_cassette(name = "Upload-vignette")
 
 ## -----------------------------------------------------------------------------
+revs <- list(
+    "biospecimen" = list(
+        "EXAMPLE-HS1-WB1" = list(biospecimen_type = "Whole Blood"),
+        "EXAMPLE-HS2-WB1" = list(biospecimen_type = "Whole Blood")
+        ),
+    "rna_seq" = list(
+        "EXAMPLE-HS1-WB1-RSQ1" = list(fraction = "Tcells")
+    )
+)
+updateValues(
+    target = prod,
+    project = "example",
+    revisions = revs,
+    auto.proceed = TRUE) ## <---
+
+## -----------------------------------------------------------------------------
 library(magmaR)
 retrieveAttributes(target = prod, "example", "biospecimen")
 retrieveAttributes(target = prod, "example", "rna_seq")
+
+## ---- eval = FALSE------------------------------------------------------------
+#  ### From a csv
+#  updateMatrix(
+#      target = prod,
+#      projectName = "example",
+#      modelName = "rna_seq",
+#      attributeName = "gene_counts",
+#      matrix = "path/to/rna_seq_counts.csv")
+#  
+#  ### From a tsv, set the 'separator' input to "\t"
+#  updateMatrix(
+#      target = prod,
+#      projectName = "example",
+#      modelName = "rna_seq",
+#      attributeName = "gene_counts",
+#      matrix = "path/to/rna_seq_counts.tsv",
+#      # Use separator to adjust parsing for tab-separated values
+#      separator = "\t")
+#  
+#  ### From an already loaded matrix:
+#  matrix <- retrieveMatrix(target = prod, "example", "rna_seq", "all", "gene_counts")
+#  updateMatrix(
+#      target = prod,
+#      projectName = "example",
+#      modelName = "rna_seq",
+#      attributeName = "gene_counts",
+#      matrix = matrix)
+
+## ---- include = FALSE---------------------------------------------------------
+matrix <- retrieveMatrix(target = prod, "example", "rna_seq", "all", "gene_counts")
+
+## -----------------------------------------------------------------------------
+head(matrix, n = c(6,2))
+
+## ---- eval = FALSE------------------------------------------------------------
+#  ### From a csv
+#  updateFromDF(
+#      target = prod,
+#      projectName = "example",
+#      modelName = "rna_seq",
+#      df = "path/to/rna_seq_attributes.csv")
+#  
+#  ### From a tsv, set the 'separator' input to "\t"
+#  updateFromDF(
+#      target = prod,
+#      projectName = "example",
+#      modelName = "rna_seq",
+#      df = "path/to/rna_seq_attributes.tsv",
+#      # Use separator to adjust parsing for tab-separated values
+#      separator = "\t")
+#  
+#  ### From an already loaded data.frame:
+#  df <- retrieve(target = prod, "example", "rna_seq", "all",
+#                 c("tube_name", "cell_number", "fraction"))
+#  updateFromDF(
+#      target = prod,
+#      projectName = "example",
+#      modelName = "rna_seq",
+#      df = df)
+
+## ---- include = FALSE---------------------------------------------------------
+df <- retrieve(target = prod, "example", "rna_seq", "all",
+               c("tube_name", "cell_number", "fraction"))
+
+## -----------------------------------------------------------------------------
+head(df, n = c(6,3))
 
 ## ---- eval = FALSE------------------------------------------------------------
 #  # Create 'revisions'
@@ -53,39 +136,6 @@ updateValues(
     project = "example",
     revisions = revs,
     auto.proceed = TRUE)
-
-## ---- eval = FALSE------------------------------------------------------------
-#  ### From a csv
-#  updateMatrix(
-#      target = prod,
-#      projectName = "example",
-#      modelName = "rna_seq",
-#      attributeName = "gene_counts",
-#      matrix = "../tests/testthat/rna_seq_counts.csv")
-#  
-#  ### From a tsv, set the 'separator' input to "\t"
-#  updateMatrix(
-#      target = prod,
-#      projectName = "example",
-#      modelName = "rna_seq",
-#      attributeName = "gene_counts",
-#      matrix = "../tests/testthat/rna_seq_counts.tsv",
-#      separator = "\t")
-#  
-#  ### From an already loaded matrix:
-#  matrix <- retrieveMatrix(target = prod, "example", "rna_seq", "all", "gene_counts")
-#  updateMatrix(
-#      target = prod,
-#      projectName = "example",
-#      modelName = "rna_seq",
-#      attributeName = "gene_counts",
-#      matrix = matrix)
-
-## ---- include = FALSE---------------------------------------------------------
-matrix <- retrieveMatrix(target = prod, "example", "rna_seq", "all", "gene_counts")
-
-## -----------------------------------------------------------------------------
-head(matrix, n = c(6,2))
 
 ## ---- include = FALSE---------------------------------------------------------
 eject_cassette()
